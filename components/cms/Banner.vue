592cc3df07ca30f0cd5b2d0ba2ccce2a824a8c17
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2022-01-12 23:26:38 +0530

    Update Banner.vue

--- components/cms/Banner.vue
+++ components/cms/Banner.vue
@@ -1,28 +1,25 @@
 <template>
-  <div>
-    {{ component }}
-    <Banner
-      :title="title"
-      :subtitle="subtitle"
-      :description="description"
-      :button-text="banner_text"
-      :link="banner_link"
-      :image="image"
-      :background="background"
-    />
-  </div>
+  <SfBanner
+    :title="title"
+    :subtitle="subtitle"
+    :description="description"
+    :button-text="button_text"
+    :link="button_link"
+    :image="image.url"
+    :background="background"
+  >
+  </SfBanner>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import { SfBanner } from "@storefront-ui/vue";
+<script>
+import Vue from 'vue'
+import { SfBanner } from '@storefront-ui/vue'
 export default {
-  name: "Banner",
+  name: 'Banner',
   components: {
-    SfBanner,
+    SfBanner
   },
-
-  props: {
+ props: {
     title: {},
     subtitle: {},
     description: {},
@@ -32,4 +29,6 @@ export default {
     background: {},
   },
 };
+ 
+
 </script>
